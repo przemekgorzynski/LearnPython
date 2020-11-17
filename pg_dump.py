d22fd81f9d892d16c8359033b4b1d864a9a8fe77
przemekgorzynski <43602911+przemekgorzynski@users.noreply.github.com>
2020-11-17 13:16:16 +0100

    Update pg_dump.py

--- pg_dump.py
+++ pg_dump.py
@@ -1,30 +1,40 @@
-# Script that makes dump of postgresql database, create it and remove ones older than two days
-# Creates file .pgpass in user's home directory with content *:*:database:username:password  and chmod 0600
-# Update : BASE64PASSWORD, *:*:database:username
+# creating backup of postgresql databse using pg_dump and remove backups older than two days  ( intentionally running from crontab )
+# Run script:  backup.py BASE64_ENCODED_PG_PASSWORD  PATH_TO_BACKUPS  eg.  backup.py  cGFzc3dvcmQ=  /var/pg_backup
+# TODO: write execution of "-h" parameter to show help
 
-import os
+import os, sys
 
-prepare_pass = """ echo "BASE64PASSWORD=" > password """                                    # Write database base64 decoded password to file
-os.system("bash -c '%s'" % prepare_pass)
+pg_password = sys.argv[1]
+log_path = sys.argv[2]
 
-pgpass_script = """ echo "*:*:database:username:"`base64 -d password`"" > ~/.pgpass """           # prepare file with pgpasswords
+if not os.path.exists(log_path):
+    os.makedirs(log_path)
+
+#print(pg_password)
+#print(log_path)
+
+f = open( 'password', 'w' )                                                             # write base64 encoded pg_password to file
+f.write(pg_password)
+f.close()
+
+pgpass_script = """ echo "*:*:awx:awx:"`base64 -d password`"" > ~/.pgpass """           # prepare pgpass file with base64 decoded pg_dump password
 os.system("bash -c '%s'" % pgpass_script)
 
-os.chmod("/root/.pgpass", 0o600)
+os.chmod("/root/.pgpass", 0o600)                                                        # Manage pgpass file
 
-env_script = """ export PGPASSFILE='~/.pgpass' """                                      # Export variable
-os.system("bash -c '%s'" % env_script)
+os.environ['"PGPASSFILE"'] = "~/.pgpass"                                                # Export needed variables
+os.environ['log_path'] = log_path
 
-pg_dump_script = """ pg_dump -h localhost -U awx --no-password awx | gzip > backup-"`date '+%Y-%m-%d-%H:%M'`".gz """    # make backup
+pg_dump_script = """ pg_dump -h localhost -U awx awx | gzip > $log_path/backup-"`date '+%Y-%m-%d-%H:%M'`".gz """          # make backup
 os.system("bash -c '%s'" % pg_dump_script)
 
-print ( "files to remove\n"  )
+#print ( "files to remove\n"  )
 
-list_script = """ if [[ $? == "0" ]]; then find . -mtime +2 -iname "backup*.gz" -exec /usr/bin/ls {} \;; fi  """        # list files older than 2 days
+list_script = """ if [[ $? == "0" ]]; then find $log_path -mtime +2 -iname "backup*.gz" -exec /usr/bin/ls {} \;; fi  """        # list files to delete -  older than 2 days
 os.system("bash -c '%s'" % list_script)
 
-old_remove_script = """ if [[ $? == "0" ]]; then find . -mtime +2 -iname "backup*.gz" -exec /usr/bin/rm {} \;; fi  """  # remove files older than 2 dys
+old_remove_script = """ if [[ $? == "0" ]]; then find $log_path -mtime +2 -iname "backup*.gz" -exec /usr/bin/rm {} \;; fi  """  # remove files older than 2 dys
 os.system("bash -c '%s'" % old_remove_script)
 
-os.remove("password")                           # cleab temporary files
+os.remove("password")                           # clean temporary files
 os.remove("/root/.pgpass")
