64c5b52319fd7cb80cfbf2449051e6a3bdb94d86
Przemek G <PrzemekG@gmail>
2020-11-26 14:52:19 +0100

    automated installation of required libraries

--- mouse_move.py
+++ mouse_move.py
@@ -1,9 +1,23 @@
 # Script moves mouse X times every 30 sec.
 
-import sys, time, datetime
+import sys
+import subprocess
+import pkg_resources
+
+
+# Check if mouse library in installed and install if not
+required = {'mouse'}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
+
+if missing:
+    python = sys.executable
+    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
+
+
+import time, datetime, mouse
 
 help_message = """
-*** TO USE THIS SCRIPT INSTALL MOUSE LIBRARY: *** \nsudo apt install python-pip3\npip3 install mouse
 
 Script moves mouse coursor X times in a loop every 60 sec. X is provided as argument
 example:
@@ -11,18 +25,19 @@ mouse_move.py 40
 
 """
 
-try:                                                                # check if atguments were passed to script
+# check if atguments were passed to script
+try:                                                                
     sys.argv[1]
 except IndexError:
     print("No args given. Run mouse_move.py --help for help")
     quit()
 
+# check first argument
 if sys.argv[1] == "--help" or sys.argv[1] == "-h":                  
     print(help_message)
     quit()
 
-import mouse
-
+# check current date
 now=datetime.datetime.now()
 print('')
 print("Start time:", now.strftime("%H:%M:%S"))
