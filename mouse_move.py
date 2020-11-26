0ecc33d5d1b4f4e42af5eb238ddaa31326a9b816
Przemek G <PrzemekG@gmail>
2020-11-26 14:20:10 +0100

    mouse mover

---
+++ mouse_move.py
@@ -0,0 +1,35 @@
+# Script moves mouse X times every 30 sec.
+import mouse, sys, time, datetime
+
+help_message = """
+*** TO USE THIS SCRIPT INSTALL MOUSE LIBRARY: *** \nsudo apt install python-pip3\npip3 install mouse
+
+Script moves mouse coursor X times in a loop every 60 sec. X is provided as argument
+example:
+mouse_move.py 40
+
+"""
+
+try:                                                                # check if atguments were passed to script
+    sys.argv[1]
+except IndexError:
+    print("No args given. Run mouse_move.py --help for help")
+    quit()
+
+if sys.argv[1] == "--help" or sys.argv[1] == "-h":                  
+    print(help_message)
+    quit()
+
+now=datetime.datetime.now()
+print('')
+print("Start time:", now.strftime("%H:%M:%S"))
+
+print("Your idle time is:", int(sys.argv[1]), "minutes")
+
+i=0
+
+while i < int(sys.argv[1]):
+   print("Minutes passed:", i)
+   mouse.move(1, 1, absolute=False, duration=0.2)
+   time.sleep(60)
+   i += 1
