27997a7355ab79b96e8901b08047ef1775fd3625
Przemyslaw Gorzynski <przemyslaw.gorzynski@protonmail.com>
2023-08-07 21:59:22 +0200

    Find and play random movie from NAS backup

---
+++ find_play.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Find and play random movie from NAS backup directory.
+import os
+import sys
+import random
+import subprocess
+
+def mount():
+    command = 'sudo mount -t cifs -o credentials=/.smbcredentials,nounix,uid=1000,gid=100,dir_mode=0770,file_mode=0660 //192.168.0.61/other /home_nas_backup'
+    try:
+        subprocess.run([command], shell=True)
+    except Exception:
+        pass
+
+def list_files(path):
+    for root, dirs, files in os.walk(path):
+    	for file in files:
+    		file_list.append(os.path.join(root,file))
+                
+def get_random(file_list):
+    return random.choice(file_list)
+
+def play_movie(movie):
+     command= "/usr/bin/vlc " + movie
+     print(command)
+     subprocess.run([command], shell=True)
+
+file_list = []
+
+try:  # check if atguments were passed to script
+    sys.argv[1]
+except:
+    path = '/home_nas_backup/new'
+
+print(path)
+mount()
+list_files(path)
+movie=get_random(file_list)
+print(movie)
+play_movie(movie)
