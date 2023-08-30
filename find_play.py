0d22cb31d50e486451ae85462466f33fa7f62567
Przemyslaw Gorzynski <przemyslaw.gorzynski@protonmail.com>
2023-08-30 22:42:23 +0200

    find_play.py update

--- find_play.py
+++ find_play.py
@@ -6,40 +6,80 @@ import random
 import subprocess
 import time
 
-def mount(path):
+HELP_MESSAGE = """
+Script usage:
+find_play.py PATH number_of_choices
+PATH - provide PATH to play file from, mandatory,  if provided 'default' = /home_nas_backup/new
+number_of_choices - how many titles you will choose file from, optional, default = 7
+"""
+
+# Mount PATH if not reacheble
+def mount(PATH):
     command = 'sudo mount -t cifs -o credentials=/.smbcredentials,nounix,uid=1000,gid=100,dir_mode=0770,file_mode=0660 //192.168.0.61/other /home_nas_backup'
-    if os.path.exists(path):
-        None
+    if os.path.exists(PATH):
+        pass
     else:
         try:
-            subprocess.run([command], shell=True)
+            subprocess.run([command], shell=True, check=False)
             time.sleep(5)
         except Exception:
             pass
 
-def list_files(path):
-    os.chdir(path)
-    for root, dirs, files in os.walk(path):
-    	for file in files:
-    		file_list.append(os.path.join(root,file))
-                
-def get_random(file_list):
-    return random.choice(file_list)
+# Make list FULL_FILE_LIST with all files within PATH
+def list_files(PATH):
+    os.chdir(PATH)
+    for root, dirs, files in os.walk(PATH):
+        for file in files:
+            FULL_FILE_LIST.append(os.path.join(root,file))
 
-def play_movie(movie):
-     command= "/usr/bin/vlc {} 2>/dev/null".format(movie)
-     subprocess.run([command], shell=True)
+# Make list CHOICE_LIST of size CHOICE_LIST_SIZE with random choices from FULL_FILE_LIST                
+def get_random(FULL_FILE_LIST, CHOICE_LIST_SIZE):
+    for i in range(CHOICE_LIST_SIZE):
+        i = random.choice(FULL_FILE_LIST)
+        CHOICE_LIST.append(i)
+    return CHOICE_LIST
 
-file_list = []
+# Print CHOICE_LIST, wait for user's choice and play choosen file
+def play_movie(CHOICE_LIST):
+    print('Choose movie:')
+    for index, i in enumerate(CHOICE_LIST, start=1):
+        print(f"{index}: {i.rsplit('/')[-1]}")
+    print("And the winner is ....")
+    choice = input()
+    if int(choice) > len(CHOICE_LIST):
+        print ("NUmber out of list range")
+        quit()
+    else:
+        print(CHOICE_LIST[int(choice)-1])
+    command= f"/usr/bin/vlc {CHOICE_LIST[int(choice)-1]} 2>/dev/null"
+    subprocess.run([command], shell=True, check=False)
 
-try:  # check if atguments were passed to script
-    path = sys.argv[1]
-except:
-    path = '/home_nas_backup/new'
+os.system("clear")
+FULL_FILE_LIST = []
+CHOICE_LIST = []
 
-print(path)
-mount(path)
-list_files(path)
-movie=get_random(file_list)
-print(movie)
-play_movie(movie)
+# Check arguments
+try:
+    sys.argv[1]
+except IndexError:
+    print("Uncorrect args given. Run find_play.py --help for help")
+    quit()
+
+if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+    print(HELP_MESSAGE)
+    quit()
+elif sys.argv[1] == "default":
+    PATH = '/home_nas_backup/new'
+else:
+    PATH = sys.argv[1]
+
+# If 2nd parameter is provided, otherwise set default value
+if sys.argv[2:]:
+    CHOICE_LIST_SIZE = int(sys.argv[2])
+else:
+    CHOICE_LIST_SIZE = 7
+
+mount(PATH)
+list_files(PATH)
+CHOICE_LIST=get_random(FULL_FILE_LIST, CHOICE_LIST_SIZE)
+play_movie(CHOICE_LIST)
